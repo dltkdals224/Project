4020b0fe8c49cad3772609dd94dff2ed340b5ae6
rlawo0307 <rlawo0307@naver.com>
2017-05-31 23:50:54 +0900

    rank 설명 추가/김지혜/게임실행 코드 작성후 재검토 필요

--- b.c
+++ b.c
@@ -47,21 +47,21 @@ int main(void)
 	printf("Hello %s",name);
 	printf("\n");
 
-    _Bool game =1;
-    while (game){
+    _Bool game =1; //게임 시작-게임 정지후 다시 시작할때, 새로운 게임 시작할때 필요
+    while (game){ //rank 5개 저장
     	int rank[5];
 		int i, j, tmp;
-		time_t start_time, final_time;
+		time_t start_time, final_time; //게임실행 시간 계산
         long long gap_time;
-		FILE *out;
+		FILE *out; //게임실행시간을 rank 파일에 저장
 		out = fopen("rank", "a");
         time(&start_time); //게임 시작 시간
         printf("123354656789876543245267"); //게임 실행코드 입력
 		time(&final_time); //게임 종료 시간
         gap_time = start_time - final_time;
-		if(gap_time>rank[4])
+		if(gap_time>rank[4]) // 새로운 기록(시간)을 기존에 저장되어있던 기록중 가장 낮은 기록과 비교하여 걸린시간이 더 짧으면 인덱스에 저장 or 걸린시간이 더 길면 값을 버림
 			rank[4] = gap_time;
-		for(i=0; i<4; i++)
+		for(i=0; i<4; i++) //저장된 인덱스 값들을 버블로 다시 정렬
 			for(j=4; j>i; j--)
 				if(rank[j-1] > rank[j]){
 					tmp = rank[j-1];
