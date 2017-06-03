a0fcf5b6b1353ea75df5623cf82937a8b21fc46f
baechaeeun <bchaeeun@naver.com>
2017-06-03 11:34:14 +0900

    배채은 움직이는거 보관함위치저장, 수정 (완성xxxx너무생각이안나서 맞게는 못한듯)

---
+++ gameflow1.c
@@ -0,0 +1,211 @@
+#include<stdio.h>
+#include<stdlib.h> //system()함수쓸때 필요
+#include<time.h> //시간함수
+#include<unistd.h>
+#include<termio.h>//getch()함수에 필요
+#define N 30
+int map[5][N][N];//전역 변수
+int name[10]; //이름받을때 쓰는거
+int p_map[30][30];
+int x,y;
+int getch()
+{
+	int ch;
+
+	struct termios buf;
+	struct termios save;
+
+	tcgetattr(0, &save);
+	buf = save;
+
+	buf.c_lflag&=~(ICANON|ECHO);
+	buf.c_cc[VMIN] = 1;
+	buf.c_cc[VTIME] = 0;
+
+	tcsetattr(0,TCSAFLUSH,&buf);
+	ch = getchar();
+	tcsetattr(0,TCSAFLUSH,&save);
+
+	return ch;
+}
+int scanchar(){
+	for(int i=0;i<N;i++)
+		for(int j=0;j<N;j++)
+			if(map[0][i][j]=='@'){
+				x=j;
+				y=i;
+			}
+	return 0;
+}
+void keyMove(){
+	int key;
+	scanchar();
+	key=getch();
+
+	system("clear");
+
+	map[0][y][x]='O'; //보관함 위치 저장 이러케하는거 절대아닌거 알지만 몰라서 일다 이렇게해써 ㅠㅠㅠㅠㅠㅠ
+
+	if(map[0][y][x]='@'){
+		map[0][y][x]='@'}
+
+	else if(map[0][y][x]='$'){
+		map[0][y][x]='$'}
+	
+	else if(map[0][y][x]='O'){
+		map[0][y][x]='O'} //보관함 위치에 다른거오면 다른거 찍히게, 아무것도 없으면 보관함 찍히게 이렇게맞니
+
+
+
+	switch(key){
+		case 'h':
+			if(map[0][y][x-1] == '$'){
+				if(map[0][y][x-2] == ' '){
+					map[0][y][x-2]='$';
+					map[0][y][x]=' ';
+					map[0][y][x-=1]='@';
+				}
+				else if(map[0][y][x-2] == 'O'){
+					map[0][y][x-2]='$';
+					map[0][y][x]=' ';
+					map[0][y][x-=1]='@';
+				}
+			}
+			else if(map[0][y][x-1] == 'O'){
+				map[0][y][x]=' '; //이부분 'O'이었는데 이상해서 ' '로 바꿨는데 맞지?밑에 3개도 그렇게 바꿔씀
+				map[0][y][x-=1]='@';
+			}
+			else if(map[0][y][x-1] == ' '){
+				map[0][y][x]=' '; 
+				map[0][y][x-=1]='@';
+			}
+			break;
+		
+		case 'j':
+			if(map[0][y+1][x] == '$'){
+				if(map[0][y+2][x] == ' '){
+					map[0][y+2][x]='$';
+					map[0][y][x]=' ';
+					map[0][y+=1][x]='@';
+				}
+				else if(map[0][y+2][x] == 'O'){
+					map[0][y+2][x]='$';
+					map[0][y][x]=' ';
+					map[0][y+=1][x]='@';
+				}
+			}
+			else if(map[0][y+1][x] == 'O'){
+				map[0][y][x]=' ';
+				map[0][y+=1][x]='@';
+			}
+			else if(map[0][y+1][x] == ' '){
+				map[0][y][x]=' ';
+				map[0][y+=1][x]='@';
+			}
+			break;
+
+		case 'k':
+			if(map[0][y-1][x] == '$'){
+				if(map[0][y-2][x] == ' '){
+					map[0][y-2][x]='$';
+					map[0][y][x]=' ';
+					map[0][y-=1][x]='@';
+				}
+				else if(map[0][y-2][x] == 'O'){
+					map[0][y-2][x]='$';
+					map[0][y][x]=' ';
+					map[0][y-=1][x]='@';
+				}
+			}
+			else if(map[0][y-1][x] == 'O'){
+				map[0][y][x]=' ';
+				map[0][y-=1][x]='@';
+			}
+			else if(map[0][y-1][x] == ' '){
+				map[0][y][x]=' ';
+				map[0][y-=1][x]='@';
+			}
+			break;
+
+		case 'l':
+			if(map[0][y][x+1] == '$'){
+				if(map[0][y][x+2] == ' '){
+					map[0][y][x+2]='$';
+					map[0][y][x]=' ';
+					map[0][y][x+=1]='@';
+				}
+				else if(map[0][y][x+2] == 'O'){
+					map[0][y][x+2]='$';
+					map[0][y][x]=' ';
+					map[0][y][x+=1]='@';
+				}
+			}
+			else if(map[0][y][x+1] == 'O'){
+				map[0][y][x]=' ';
+				map[0][y][x+=1]='@';
+			}
+			else if(map[0][y][x+1] == ' '){
+				map[0][y][x]=' ';
+				map[0][y][x+=1]='@';
+			}
+			break;
+	}
+
+}
+int mapscan()
+{
+	FILE *scan;		
+	int a;
+	int x=0,y=0,z=0,i;
+	scan=fopen("map5.txt","r");
+	while(fscanf(scan,"%c",&a) != EOF){
+		if(a=='\n'){
+			y++;
+			x=0;
+			}
+		else if(a=='$'){
+			map[z][y][x]='$';
+			x++;}
+		else if(a=='@'){
+			map[z][y][x]='@';
+			x++;}
+		else if(a=='#'){
+			map[z][y][x]='#';
+			x++;}
+		else if(a==' '){
+			map[z][y][x]=' ';
+			x++;}
+		else if(a=='O'){
+			map[z][y][x]='O';
+			x++;}
+	}
+	fclose(scan);
+	return 0;
+}
+int mapprint()
+{
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++)
+			printf("%c",map[0][i][j]);
+		printf("\n");
+	}
+	return 0;
+}
+int main()
+{
+	int q=0;
+	for(int i=0;i<N;i++)
+		for(int j=0;j<N;j++)
+			map[0][i][j]=' ';
+	system("clear");
+	mapscan();
+
+	mapprint();
+	while(1){
+		keyMove();
+		mapprint();
+	}
+	return 0;
+}
+
+
