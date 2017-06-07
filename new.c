95f8d83b6bde326a10a3b4de022fab71522fc54a
rlawo0307 <rlawo0307@naver.com>
2017-06-08 00:44:34 +0900

    김지혜 // 옵션 r,n,e// 완성도: 개미꼬딱지만큼

---
+++ new.c
@@ -0,0 +1,55 @@
+void key_n()
+{
+    if key('n')
+        d_diff1=0;
+        d_diff2=0;
+        d_diff3=0;
+        d_diff4=0;
+        d_diff5=0;
+        d_diff_all=0;
+
+	    system("clear");
+	
+	    mapscan();
+	    mapprint1();
+	    map1();
+	    mapprint2();
+	    map2();
+	    mapprint3();
+	    map3();
+	    mapprint4();
+	    map4();
+	    mapprint5();
+	    map5();
+
+	    rank_game();
+	    printf("축하메세지 출력");
+}
+
+void key_e
+{
+	if key('e')
+		file_save();
+	    whole_game=0;
+}
+
+void key_r
+{
+	int a;
+	if key('r'){
+		a=0;
+		if ((d_diff2==0)&&(d_diff3==0)&&(d_diff4==0)&&(d_diff5==0)){
+			system("clear");
+			mapprint1();
+			if(게임을 완료할 조건){}
+		
+
+            
+		        
+
+		}
+
+
+
+	
+
